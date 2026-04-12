fc77623e89a9fb191e127c32be2ff69ecf3c6456
safdev758 <sa.khentit@esi-sba.dz>
2026-04-12 18:53:08 +0100

    fix(env): add https protocol to api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api/v1'
+  apiUrl: 'https://layanbackend-production.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'layanbackend-production.up.railway.app'
+  apiUrl: 'https://layanbackend-production.up.railway.app'
 };
